4ab86e5a44ab66459f526b042626c135d3665608
radman <109485761+rnd2131@users.noreply.github.com>
2024-08-19 01:31:16 +0330

    sw

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-(function(d, t) {
-    var g = d.createElement(t),
-    s = d.getElementsByTagName(t)[0];
-    g.src = "https://cdn.pushalert.co/integrate_4598859a70f21d0b1be28285e3c10882.js";
-    s.parentNode.insertBefore(g, s);
-}(document, "script"));
+importScripts("https://cdn.pushalert.co/sw-73330.js");
